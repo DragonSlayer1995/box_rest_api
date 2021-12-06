1b7c6af76000513c54da47be0e369508cfe8a0f0
Pavel Strakhov <p.strakhov4work@gmail.com>
2021-12-06 19:08:35 +0300

    Add get and post methods. Basic checks for post method. Need refactoring

--- box_list.py
+++ box_list.py
@@ -1,5 +1,5 @@
 import re
-from uuid import UUID
+from datetime import datetime, timedelta
 from box import Box
 
 first_box = Box('2f11868c-a367-48dc-b4d1-c6706f5258f4', 'simple box', 12, '2021-10-15')
@@ -12,3 +12,22 @@ ALL_BOXES = {
 def is_guid_valid(guid):
     guid_pattern = re.compile(r'^[\da-f]{8}-([\da-f]{4}-){3}[\da-f]{12}$', re.IGNORECASE)
     return True if guid_pattern.match(guid) else False
+
+
+def validate_box_name(box_name):
+    result = False
+    if box_name:
+        if 5 <= len(box_name) <= 30:
+            result = True
+    return result
+
+
+def validate_box_make_date(box_make_date):
+    result = False
+    current_date = datetime.today().date()
+    current_date_minus_year = current_date - timedelta(days=365)
+    if box_make_date:
+        make_date = datetime.strptime(box_make_date, '%Y-%m-%d').date()
+        if current_date_minus_year <= make_date <= current_date:
+            result = True
+    return result
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from flask import Flask, abort, request
-from box_list import ALL_BOXES, is_guid_valid
+from flask import Flask, abort, request, Response
+from box_list import ALL_BOXES, is_guid_valid, validate_box_name, validate_box_make_date
 
 app = Flask(__name__)
 
@@ -22,6 +22,12 @@ def get_box_by_id(box_id):
 @app.route('/api/box', methods=['POST'])
 def create_box():
     body = request.get_json()
+    if body:
+        print('Body is not empty')
+        if validate_box_name(body['name']) and validate_box_make_date(body['make_date']):
+            return 'box was created'
+        else:
+            return Response(f"Problems with name: {body['name']}", status=400)
 
 
 if __name__ == '__main__':
